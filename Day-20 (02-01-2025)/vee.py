0f77062b03385ebbcf42635ad7ecb798b7a584a6
Milan Das <45796126+milandas63@users.noreply.github.com>
2025-01-02 17:32:09 +0530

    Add files via upload

---
+++ Day-20 (02-01-2025)/vee.py
@@ -0,0 +1,32 @@
+"""
+
+            1               1
+            12             21
+            123           321
+            1234         4321
+            12345       54321
+            123456     654321
+            1234567   7654321
+            12345678 87654321
+            12345678987654321
+
+"""
+
+spaces = 17
+for l in range(1,11):
+    for s in range(0,20):
+        print(" ", end="")
+    for t in range(1,l):
+        print(t, end="")
+    for s in range(0,spaces):
+        print(" ", end="")
+    if l<10:
+        k = l-1
+    else:
+        k = l-2
+    for t in range(k,0,-1):
+        print(t, end="")
+    spaces = spaces - 2
+    print()
+
+
